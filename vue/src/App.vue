a94db2febe5cd3a3c29c78b88b4fccff3ed257e2
angelo friello <angelofriello01@gmail.com>
2023-11-10 14:06:16 +0100

    add vite

---
+++ vue/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+    import HeaderApp from './components/HeaderApp.vue';
+    import MainApp from './components/MainApp.vue';
+    import FooterApp from './components/FooterApp.vue';
+    import AppCard from './components/AppCard.vue';
+    import { store } from './store.js';
+    import axios from 'axios';
+
+    export default {
+        components:{
+            HeaderApp,
+            MainApp,
+            FooterApp,
+        },
+        data() {
+            return{
+                store: store,
+
+            }
+        },
+        created(){
+
+        },
+    } 
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
---
+++ vue/src/components/MainApp.vue
@@ -0,0 +1,11 @@
+<script>
+    import AppCard from './AppCard.vue';
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
