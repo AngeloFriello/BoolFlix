e30595d709412c74626f9a5fb41c69a2fbd2a06a
angelo friello <angelofriello01@gmail.com>
2023-11-13 17:42:45 +0100

    add date

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -23,11 +23,12 @@
 </script>
 
 <template>
-    <img :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt="" class="cover">
+    <img  :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt="" class="cover">
     <ul class="info">
-        <li>Nome Originale : {{ item.original_title }} {{ item.original_name }}</li>
+        <li >Nome Originale : {{ item.original_title }} {{ item.original_name }}</li>
         <li>Nome : {{ item.title }} {{ item.name }}</li>
         <!-- <li>Descrizione : {{ item.overview }}</li> -->
+        <li>{{ item.release_date }} {{ item.first_air_date }}</li>
         <li class="flex">
             <p>>Voto :</p>
             <p v-for="star of (Math.round(item.vote_average / 2))"><font-awesome-icon :icon="['fas', 'star']" class="star"/></p>
