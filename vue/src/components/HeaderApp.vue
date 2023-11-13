df0b0186ef87fa6ef3a97588b91b4f721e732995
angelo friello <angelofriello01@gmail.com>
2023-11-13 11:42:04 +0100

    ad style basic

--- vue/src/components/HeaderApp.vue
+++ vue/src/components/HeaderApp.vue
@@ -10,22 +10,12 @@
 </script>
 
 <template>
-    <div class="ricerca">
-        <input class="search" type="text" v-model="store.searchText" placeholder="cerca" @keyup.enter="$emit('search')">     
+    <div>
+        <input class="search" type="text" v-model="store.searchText" placeholder="cerca" @keyup.enter="$emit('search')">
     </div>
+    <h1 class="title">BOOLFLIX</h1>
 </template>
 
 <style>
-    .ricerca{
-        width: 1200px;
-        margin: 0px 1500px;
-        
-    }
 
-    .button{
-        background-color: red;
-        width: 20px;
-        height: 20px;
-        margin: 10px;
-    }
 </style>
